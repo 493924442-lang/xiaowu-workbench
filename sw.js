5326b42aef77434b9364024751b4e1d5bfe82478
小吴工作台部署 <deploy@workbuddy.local>
2026-07-30 22:30:38 +0800

    全屏显示+日历(阳历农历)+英语每日练习(拼读/跟读/打卡,10词10句)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== 小吴工作台 Service Worker =====
-const CACHE_NAME = 'xiaowu-workbench-v10-pop';
+const CACHE_NAME = 'xiaowu-workbench-v11-fullscreen-calendar-en';
 const ASSETS = [
   './',
   './index.html',
